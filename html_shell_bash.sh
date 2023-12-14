2d2b4a62bc3303c3432caba35dcebeeced80c594
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-12-14 05:11:23 +0100

    Update html_shell_bash.sh

--- html_shell_bash.sh
+++ html_shell_bash.sh
@@ -1,1 +1,15 @@
 <? #SHELL_SCRIPT# >
+
+## works best when run as bash script
+
+<!-- ; tput cuu 2; tput ed; ## BASH SHELL INSIDE #######
+########################################################
+echo gg
+echo gg
+########################################################
+read -ep "END " -i "OK" "ok"; exit 1; ## bash end ## -->
+
+<!-- ; tput cuu 2; tput ed; ## BASH SHELL INSIDE #######
+echo ooooooooooooooo
+read -ep "END " -i "OK" "ok"; exit 1; ## bash end ## -->
+
